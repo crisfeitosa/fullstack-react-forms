66db1277db5671f7a14f25bb4c0ea4f986eb8608
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-01 12:14:12 -0300

    Textarea

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ type FormData = {
   name: string;
   date: string;
   subject: string;
+  description: string;
 }
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
       name: "",
       date: "",
       subject: "",
+      description: "",
     }
   });
 
@@ -70,7 +72,18 @@ export default function App() {
           )}
         />
 
-        <textarea placeholder="Descrição" rows={4} />
+        <Controller
+          control={control}
+          name="description"
+          render={({ field }) => (
+            <textarea
+              placeholder="Descrição"
+              rows={4}
+              {...field}
+            />
+          )}
+        
+        />
 
         <button type="submit">
           Salvar
