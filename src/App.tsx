7cb996c0f0789094d5050f2d3186d78c641fd057
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-01 11:57:25 -0300

    Tipagem do Formulário

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { Controller, useForm } from "react-hook-form";
 
 import "./App.css";
 
+type FormData = {
+  name: string;
+}
+
 export default function App() {
-  const { control, handleSubmit } = useForm({
+  const { control, handleSubmit } = useForm<FormData>({
     defaultValues: {
       name: ""
     }
   });
 
-  function onSubmit(data: any) {
+  function onSubmit(data: FormData) {
     console.log(data);
   }
 
