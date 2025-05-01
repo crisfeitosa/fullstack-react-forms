a161f02e36dd2ddb050cce24c1a5b149c8c3dbb5
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-01 12:26:32 -0300

    Exibindo Erros de Validação

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,14 @@ const schema = yup.object({
   name: yup.string().required("Nome é obrigatório"),
   date: yup.string().required("Data é obrigatória"),
   subject: yup.string().required("Selecione um assunto"),
-  description: yup.string().required("Descrição é obrigatória").min(10, "A descrição precisa ter pelo menos 10 caracteres"),
+  description: yup
+    .string()
+    .required("Descrição é obrigatória")
+    .min(10, "A descrição precisa ter pelo menos 10 caracteres"),
 })
 
 export default function App() {
-  const { control, handleSubmit } = useForm<FormData>({
+  const { control, handleSubmit, formState: { errors } } = useForm<FormData>({
     defaultValues: {
       name: "",
       date: "",
@@ -50,7 +53,7 @@ export default function App() {
           )}
         />
 
-        <span className="error">Nome é obrigatório</span>
+        {errors.name && <span className="error">{errors.name.message}</span>}
 
         <Controller
           control={control}
@@ -64,6 +67,8 @@ export default function App() {
             />
           )}
         />
+
+        {errors.date && <span className="error">{errors.date.message}</span>}
 
         <Controller
           control={control}
@@ -82,6 +87,10 @@ export default function App() {
           )}
         />
 
+        {errors.subject && (
+          <span className="error">{errors.subject.message}</span>
+        )}
+
         <Controller
           control={control}
           name="description"
@@ -92,8 +101,11 @@ export default function App() {
               {...field}
             />
           )}
-        
         />
+
+        {errors.description && (
+          <span className="error">{errors.description.message}</span>
+        )}
 
         <button type="submit">
           Salvar
