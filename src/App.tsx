aa13e9e4f8cf680c934b17a722b8361c632d226c
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-01 11:38:06 -0300

    Adicionando Controller no Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+
 import "./App.css";
 
 export default function App() {
-  const [name, setName] = useState("");
+  const { control, handleSubmit } = useForm();
 
-  function onSubmit(e: React.FormEvent<HTMLElement>) {
-    e.preventDefault();
-
-    setName("");
+  function onSubmit(data: any) {
+    console.log(data);
   }
 
   return (
     <div>
       <h1>Evento</h1>
 
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="Nome do evento"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="name"
+          render={({ field }) => (
+            <input
+              type="text"
+              placeholder="Nome do evento"
+              {...field}
+            />
+          )}
         />
+
         <span className="error">Nome é obrigatório</span>
 
         <input type="date" placeholder="Nome do evento" lang="pt-BR" />
