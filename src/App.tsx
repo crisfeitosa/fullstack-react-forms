8ebc4d4d10ee9d435df9a1a8cc99ddbffdfa0ca6
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-01 12:20:21 -0300

    Utilizando Schema na Validação

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,10 @@ type FormData = {
 };
 
 const schema = yup.object({
-
+  name: yup.string().required("Nome é obrigatório"),
+  date: yup.string().required("Data é obrigatória"),
+  subject: yup.string().required("Selecione um assunto"),
+  description: yup.string().required("Descrição é obrigatória").min(10, "A descrição precisa ter pelo menos 10 caracteres"),
 })
 
 export default function App() {
